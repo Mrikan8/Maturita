a56df66f6e40363db60e104c307f3dd1f6727a78
Rivercz <179596726+Mrikan8@users.noreply.github.com>
2024-09-11 21:35:50 +0200

    Add files via upload

---
+++ 1. PŘEVOD ČASU/prevodcasu.py
@@ -0,0 +1,13 @@
+print('Zadej čas v sekundách: ')
+cas = int(input())
+
+dny = cas // 86400
+zbyvajici_sekundy = cas % 86400
+
+hodiny = zbyvajici_sekundy // 3600
+zbyvajici_sekundy %= 3600
+
+minuty = zbyvajici_sekundy // 60
+sekundy = zbyvajici_sekundy % 60
+
+print(f'{cas} sekund je {dny} dní, {hodiny} hodin, {minuty} minut a {sekundy} sekund.')
