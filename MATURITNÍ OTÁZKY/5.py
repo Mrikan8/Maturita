c50b59b2f1ae4c92d878189475fe18b4d14512e2
Rivercz <hotelsustr1@gmail.com>
2024-10-16 11:39:22 +0200

    dodelane priklady

--- MATURITNÍ OTÁZKY/5.py
+++ MATURITNÍ OTÁZKY/5.py
@@ -1,1 +1,12 @@
-print('ahoj')
+a = int(input("Zadejte první číslo: "))
+b = int(input("Zadejte druhé číslo: "))
+c = int(input("Zadejte třetí číslo: "))
+
+if a > b:
+    a, b = b, a
+if b > c:
+    b, c = c, b
+if a > b:
+    a, b = b, a
+
+print(c, b, a)
--- MATURITNÍ OTÁZKY/6.py
+++ MATURITNÍ OTÁZKY/6.py
@@ -1,1 +1,13 @@
-# sestavte algoritmus pro výpočet součinu přirozených čísel menších nebo rovných N
+n = int(input('Zadej číslo n: '))
+c = int(input('Zadej číslici, kterou chceš v n vyhledat: '))
+
+n_str = str(n)
+c_str = str(c)
+
+if c_str in n_str:
+    print(f"Číslice {c} se v čísle {n} vyskytuje.")
+else:
+    print(f"Číslice {c} se v čísle {n} nevyskytuje.")
+
+p = n_str.count(c_str)
+print(f"Počet výskytů číslice {c} v čísle {n} je {p}.")
