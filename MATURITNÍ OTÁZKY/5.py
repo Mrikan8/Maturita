43400dc3689ece7a48561a012cabe4061a98ca27
Rivercz <hotelsustr1@gmail.com>
2024-10-16 11:23:14 +0200

    ahoj

--- MATURITNÍ OTÁZKY/5.py
+++ MATURITNÍ OTÁZKY/5.py
@@ -1,7 +1,1 @@
-while True:
-    heslo = int(input('Zadej heslo: '))
-    if heslo == 1234:
-        print('Správné heslo')
-        break
-    else:
-        print('Špatné heslo, zkuste to znovu.')
+print('ahoj')
